3273a324ee2b0092c3f9cb5f4901da076173bf73
Sebastian <sa224ny@student.lnu.se>
2022-03-21 15:12:18 +0100

    Add: views and default layout

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 import express from 'express'
+import expressLayouts from 'express-ejs-layouts'
 import morgan from 'morgan'
-
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 const main = async () => {
   const port = process.env.PORT || 5000
+  const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
   const server = express()
+
   server.use(morgan('dev'))
 
+  server.set('view engine', 'ejs')
+  server.set('views', join(directoryFullName, 'views'))
+  server.use(expressLayouts)
+  server.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+  server.use(express.static(join(directoryFullName, '..', 'public')))
+
+  server.use(express.urlencoded({ extended: false }))
+
   server.get('/', (req, res) => {
-    res.send('Seems to work...')
+    res.render('layouts/default')
   })
 
   server.listen(port, () => {
