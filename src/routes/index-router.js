3a41a578270bf491e39e9845fdc1d66cab2cf97e
Sebastian <sa224ny@student.lnu.se>
2022-03-21 16:38:49 +0100

    Add: Some styling

--- src/routes/index-router.js
+++ src/routes/index-router.js
@@ -2,10 +2,6 @@ import express from 'express'
 
 export const router = express.Router()
 
-router.get('/login', (req, res, next) => {
-  res.render('pages/login')
-})
-
 router.get('/activity', (req, res, next) => {
   res.render('pages/activity')
 })
