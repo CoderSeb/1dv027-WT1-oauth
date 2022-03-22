0fbb74e1e552d479ab5d88d27f936f58b5887cbb
Sebastian <sa224ny@student.lnu.se>
2022-03-22 14:50:36 +0100

    Refactor: Moved auth specific methods to authController

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,11 +1,5 @@
 
 export default class AuthController {
-  async showProfile(req, res, next) {
-    const viewData = req.session.user
-    console.log(viewData)
-    res.render('pages/profile', { viewData })
-  }
-
   async logout(req, res, next) {
     req.session.destroy((err) => {
       if (err) {
@@ -16,11 +10,6 @@ export default class AuthController {
   }
 
   async redirectProfile(req, res, next) {
-    res.redirect('/profile')
-  }
-
-  async showActivities(req, res, next) {
-    const viewData = req.session.user
-    res.render('pages/activities', { viewData })
+    res.redirect('/user/profile')
   }
 }
